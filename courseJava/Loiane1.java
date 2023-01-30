f7047d7dd940a4e85fe980145a318e6386d6602c
Samuel Moreira <samuelhmrocha@gmail.com>
2023-01-30 10:17:56 -0300

    Solicita numero e imprime o mesmo

--- courseJava/Loiane1.java
+++ courseJava/Loiane1.java
@@ -1,7 +1,11 @@
 package courseJava;
-
+import java.util.Scanner;
 public class Loiane1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    Scanner rd = new Scanner(System.in);
+
+    System.out.print("Digite um número meu nobre:");
+    int numero = rd.nextInt();
+    System.out.printf("O número que digitou foi: %d%n",numero);
     }
 }
