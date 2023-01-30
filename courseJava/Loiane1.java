0172df48b6191ca8079f3b06ddddf86ecb2de6b0
Samuel Moreira <samuelhmrocha@gmail.com>
2023-01-30 16:04:47 -0300

    Switch exercice

--- courseJava/Loiane1.java
+++ courseJava/Loiane1.java
@@ -3,13 +3,38 @@ import java.util.Scanner;
 public class Loiane1 {
     public static void main(String[] args) {
     Scanner rd = new Scanner(System.in);
-    System.out.println("Informe a área que será pintada");
-    double areaPintada = rd.nextInt();
-    double litro= areaPintada/6.0;
-    if(litro<= 3.6){
-        System.out.println("Você pode comprar apenas um galão de 3.6l. ");
-    } else if (litro<=18.0) {
-        System.out.println("Você pode comprar apenas um galão de 18l. ");
+    int contador = 0;
+    System.out.println("Telefonou para a vítima");
+    int resposta = rd.nextInt();
+    if(resposta != 1){
+        contador+= 1;
     }
-    }
+        System.out.println("Esteve no local do crime:");
+        resposta = rd.nextInt();
+        if(resposta != 1){
+            contador+= 1;
+        }
+        System.out.println("Mora perto da vítima");
+       resposta = rd.nextInt();
+        if(resposta != 1){
+            contador+= 1;
+        }
+        System.out.println("Devia para a vítima");
+      resposta = rd.nextInt();
+        if(resposta != 1){
+            contador+= 1;
+        }
+        System.out.println("Trabalha com a vítima");
+       resposta = rd.nextInt();
+        if(resposta != 1){
+            contador+= 1;
+        }
+
+            switch (contador){
+                case 2: System.out.println("Suspeita"); break;
+                case 3:
+                case 4: System.out.println("Cumplice");break;
+                case 5: System.out.println("Culpado");break;
+        }
+        }
 }
