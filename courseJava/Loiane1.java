d4a4e4072ece99fee802280ac4c42361e2b64031
Samuel Moreira <samuelhmrocha@gmail.com>
2023-01-30 10:48:15 -0300

    Imprime uma média de 4 numeros

--- courseJava/Loiane1.java
+++ courseJava/Loiane1.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Loiane1 {
     public static void main(String[] args) {
     Scanner rd = new Scanner(System.in);
-
-    System.out.print("Digite um número meu nobre:");
-    int numero = rd.nextInt();
-    System.out.print("Agora digite outro minha autarquia:");
-    int numero1 = rd.nextInt();
-    int soma = numero1+numero;
-    System.out.printf("A soma dos números digitados foi: %d%n",soma);
+    System.out.println("Este programa imprimirá a sua média anual");
+    int soma = 0;
+    int nota;
+    for (int i = 1; i <=4; i++){
+    System.out.printf("Digite a sua %d nota:",i);
+    nota = rd.nextInt();
+    soma += nota;
+    }
+    int media = soma/4;
+    System.out.printf("Sua média é %d meu caro",media);
     }
 }
