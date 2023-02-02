73670ec510846dd474206f8647a74aed7c4b2387
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-02 08:48:33 -0300

    Exercice 'DO'

--- Course.java
+++ Course.java
@@ -3,13 +3,27 @@ public class Course {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-        
-       System.out.println("Lojas Quase Dois");
-       for (int i =1; i<=50; i++) {
-    	   double preco = i * 1.99;
-    	   System.out.printf("%d - R$ %.2f%n", i,preco);
-       }
-        
+     
+		System.out.println("Lojas Tabajara");
+		double price = 0;
+		double sum = 0;
+		double cash = 0;
+		double troco =0;
+		do{
+			
+			int i =1;
+		    System.out.printf("Digite o preço do %d produto: %n",i);
+		    price = scanner.nextDouble();
+		    sum += price;  
+		    i++;
+		}while(price !=0);
+			
+			System.out.println("Dinheiro: ");
+			cash = scanner.nextDouble();
+			
+			troco = cash-sum;
+			System.out.println("Troco: "+troco);
+		
         scanner.close();
 	}
 
