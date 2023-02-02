acd64858ed1debf30644e4306c1d67bf75401512
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-02 08:24:28 -0300

    Exercice 'For'

--- Course.java
+++ Course.java
@@ -4,17 +4,11 @@ public class Course {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
         
-        double pop1 = 80000;
-        double pop2 = 2000000;
-        int cont = 0;
-        
-        while (pop1<=pop2) {
-        	pop1 = pop1*0.3;
-        	pop2 = pop2*0.15;
-        	cont +=1;
-        }
-     
-        System.out.println(cont);
+       System.out.println("Lojas Quase Dois");
+       for (int i =1; i<=50; i++) {
+    	   double preco = i * 1.99;
+    	   System.out.printf("%d - R$ %.2f%n", i,preco);
+       }
         
         scanner.close();
 	}
