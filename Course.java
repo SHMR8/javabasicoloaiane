19284a2799503f0eccd8863437b5efa8a6792409
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-02 21:33:47 -0300

    Exercice Array - calculate pairs

--- Course.java
+++ Course.java
@@ -4,12 +4,17 @@ public class Course {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
      
-		int[] A = new int[8];
-	    int[] B = new int[8];
-		for (int i = 0; i< A.length;i++) {
-		B[i]=A[i]*2;	
+		int[] A = new int[10];
+		int cont = 0;
+		for (int i =0; i<A.length;i++) {
+			System.out.println("Digite o "+(i+1)+"º valor: ");
+			A[i]=scanner.nextInt();
 		}
-		
+		for(int i =0; i<A.length;i++) {
+			if(A[i]%2 != 0) {
+			cont +=1;}
+		}
+		System.out.println("Esta é a quantidade de números pares: "+ cont);
         scanner.close();
 	}
 
