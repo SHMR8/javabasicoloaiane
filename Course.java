78c5eac3278e6670234cf105e7b5d679c98465b2
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-02 21:17:20 -0300

    Exercice 'Array-1'

--- Course.java
+++ Course.java
@@ -4,25 +4,11 @@ public class Course {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
      
-		System.out.println("Lojas Tabajara");
-		double price = 0;
-		double sum = 0;
-		double cash = 0;
-		double troco =0;
-		do{
-			
-			int i =1;
-		    System.out.printf("Digite o preço do %d produto: %n",i);
-		    price = scanner.nextDouble();
-		    sum += price;  
-		    i++;
-		}while(price !=0);
-			
-			System.out.println("Dinheiro: ");
-			cash = scanner.nextDouble();
-			
-			troco = cash-sum;
-			System.out.println("Troco: "+troco);
+		int[] A = new int[5];
+	    int[] B = new int[5];
+		for (int i = 0; i< A.length;i++) {
+		B[i]=A[i];	
+		}
 		
         scanner.close();
 	}
