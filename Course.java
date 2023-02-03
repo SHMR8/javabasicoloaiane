2e19efc7e7b309cdc2163b561add80e7fd2231d5
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-03 11:39:48 -0300

    Sort Exercice

--- Course.java
+++ Course.java
@@ -4,30 +4,31 @@ public class Course {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		int t =0;
+		int count =0;
        
 		int[] A = new int[5];
 		for(int i=0; i<A.length; i++) {
-	    System.out.println("Digite o valor:");
+	    System.out.println("Digite o valor"+ ":");
 		 A[i]=scanner.nextInt();
 		}
 		
+		int[] B = new int[5];
 		
-		
-		for (int i=0; i<A.length-1;i++) {
-			for (int j=i+1; j< A.length; j++) {
-			if(A[i] > A[j]){
-				t = A[i];
-				A[i]=A[j];
-				A[j]=t;
-				
-		  }
-		 }
+		for(int i = 0; i < A.length; i++) {
+			if(A[i]%2 == 0) {
+				B[count]=A[i];
+				count+=1;
+			}
+		}
+		for(int i = 0; i < A.length; i++) {
+			if(A[i]%2 != 0) {
+				B[count]=A[i];
+				count+=1;
+			}
 		}
 		
-		
 			for(int i =0; i<A.length; i++) {
-		System.out.println(A[i]);
+		System.out.println(B[i]);
 				}
 			}
 		
